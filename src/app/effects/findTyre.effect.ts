d2e7406409720d6dc469553d19808bad4cbcd692
vickymodi951 <vickymodi951@gmail.com>
2021-05-11 07:22:42 -0700

    efect new version used

--- src/app/effects/findTyre.effect.ts
+++ src/app/effects/findTyre.effect.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Actions,  createEffect, ofType } from '@ngrx/effects';
 import { catchError, delay, map, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { of } from 'rxjs';
@@ -10,8 +10,7 @@ import { HttpServiceService } from '../services/http-service.service'
 export class TyreEffects {
   constructor(private actions$: Actions, private api: HttpServiceService) {}
 
-  @Effect()
-  loadYears = this.actions$.pipe(
+  loadYears = createEffect(() => this.actions$.pipe(
     ofType(findTyreActions.YEAR_LIST_REQUEST),
     switchMap(action => {
       return this.api
@@ -27,10 +26,9 @@ export class TyreEffects {
           catchError(error => of(new findTyreActions.YearListsFailureAction(error)))
         );
     })
-  );
+  ));
 
-  @Effect()
-  loadMakes = this.actions$.pipe(
+  loadMakes = createEffect(() =>this.actions$.pipe(
     ofType(findTyreActions.MAKE_LIST_REQUEST),
     switchMap(action => {
       return this.api.get('/makes').pipe(
@@ -42,10 +40,9 @@ export class TyreEffects {
         catchError(error => of(new findTyreActions.MakeListsFailureAction(error)))
       );
     })
-  );
+  ));
 
-  @Effect()
-  loadModels = this.actions$.pipe(
+  loadModels = createEffect(() =>this.actions$.pipe(
     ofType(findTyreActions.MODEL_LIST_REQUEST),
     switchMap(action => {
       return this.api.get('/models').pipe(
@@ -57,10 +54,9 @@ export class TyreEffects {
         catchError(error => of(new findTyreActions.ModelListsFailureAction(error)))
       );
     })
-  );
+  ));
 
-  @Effect()
-  loadTrims = this.actions$.pipe(
+  loadTrims = createEffect(() =>this.actions$.pipe(
     ofType(findTyreActions.TRIM_LIST_REQUEST),
     switchMap(action => {
       return this.api.get('/trim').pipe(
@@ -72,5 +68,5 @@ export class TyreEffects {
         catchError(error => of(new findTyreActions.TrimListsFailureAction(error)))
       );
     })
-  );
+  ));
 }
